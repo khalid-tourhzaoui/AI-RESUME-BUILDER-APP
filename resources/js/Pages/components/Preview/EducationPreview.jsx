9e91bf879ca9ec5b4d1a6bf20a00172dc3db9ac1
khalid-tourhzaoui <tarikkhalidoss100v3356@gmail.com>
2024-12-20 21:51:44 +0000

    Education && Skill Preview

--- resources/js/Pages/components/Preview/EducationPreview.jsx
+++ resources/js/Pages/components/Preview/EducationPreview.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import SkeletonLoader from "../skeleton-loader";
+import { INITIAL_THEME_COLOR } from "@/lib/helper";
 
-function EducationPreview() {
-  return (
-    <div>EducationPreview</div>
-  )
+function EducationPreview({document, isLoading}) {
+    const themeColor = document?.themeColor || INITIAL_THEME_COLOR;
+
+    if (isLoading) {
+        return <SkeletonLoader />;
+    }
+    return (
+        <div className="w-full my-5">
+            <h5 className="text-center font-bold mb-2" style={{ color: themeColor }}>
+                Education
+            </h5>
+            <hr className="border-[1.5px] my-2" style={{borderColor: themeColor}}/>
+
+            <div className="flex flex-col gap-2 min-h-9">
+                {document?.education?.map((item, index) => (
+                    <div key={index}>
+                        <h5
+                            className="text-sm font-bold"
+                            style={{ color: themeColor }}
+                        >
+                            {item?.universityName}
+                        </h5>
+                        <div className="flex items-start justify-between">
+                            <h5 className="text-[13px]">
+                                {item?.degree}
+                                {item?.degree &&
+                                    item?.major &&
+                                    " in "}
+                                {item?.major}
+                            </h5>
+                            <span className="text-[13px]">
+                                {item?.start_date}
+                                {item?.start_date && " - "}
+                                {item?.end_date}
+                            </span>
+                        </div>
+                        <p className="text-[13px] my-2">
+                            {item?.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default EducationPreview
+export default EducationPreview;
--- resources/js/Pages/components/Preview/SkillPreview.jsx
+++ resources/js/Pages/components/Preview/SkillPreview.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import SkeletonLoader from "../skeleton-loader";
+import { INITIAL_THEME_COLOR } from "@/lib/helper";
 
-function SkillPreview() {
-  return (
-    <div>SkillPreview</div>
-  )
+function SkillPreview({document, isLoading}) {
+    const themeColor = document?.themeColor || INITIAL_THEME_COLOR;
+
+    if (isLoading) {
+        return <SkeletonLoader />;
+    }
+    return (
+        <div className="w-full my-5">
+            <h5 className="text-center font-bold mb-2" style={{ color: themeColor }}>
+                Skills
+            </h5>
+            <hr className="border-[1.5px] my-2" style={{borderColor: themeColor,}}/>
+
+            <div className="grid grid-cols-2  gap-3 pt-3 my-1 min-h-9">
+                {document?.skills?.map((skill, index) => (
+                    <div key={index}
+                        className="flex items-center justify-between">
+                        <h5 className="text-[13px]">{skill?.name}</h5>
+                        {skill?.rating && skill?.name ? (
+                            <div className="h2 bg-gray-200 w-[120px]">
+                                <div
+                                    className="h-2"
+                                    style={{
+                                        background: themeColor,
+                                        width: skill?.rating * 20 + "%",
+                                    }}
+                                />
+                            </div>
+                        ) : null}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default SkillPreview
+export default SkillPreview;
